d1e128bf8f78b8945dbe6a0b5f926281fb1c9f86
TwistedPrime <36546624+TwistedPrime@users.noreply.github.com>
2022-05-23 12:37:31 +0100

    Revert "msm_thermal_simple: Disable super cool by default"
    This reverts commit dd2b03754be18f61cc52948fd636d9a2b5bdd9ca.

--- drivers/thermal/qcom/msm_thermal_simple.c
+++ drivers/thermal/qcom/msm_thermal_simple.c
@@ -14,7 +14,7 @@
 #include <linux/thermal.h>
 #include <linux/moduleparam.h>
 
-bool super_cool = false;
+bool super_cool = true;
 module_param(super_cool, bool, 0644);
 
 #define OF_READ_U32(node, prop, dst)						\
